ee23c188b3783486ace3f3615602d86d4c36e8bb
walttoncoelho <walltton@outlook.com>
2023-06-27 20:04:26 -0300

    ADICIONA O PLUGIN DE DEPLOY NETLIFY

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+    "netlify-deployments": {
+      enabled: true,
+      config: {
+        accessToken: "<RI8DUhQqvYxQukjg56LcGKuqKdMW2H-CQzEjy-z6plo>",
+        sites: [
+          {
+            name: 'cromo-back',
+            id: "20e50cf8-4570-4cef-9420-b715304c8a6d",
+            buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            branch: 'main' // optional
+          }
+        ]
+      },
+    },
+  });
